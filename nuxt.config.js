e57ce9630a95190b0cda9d896d0f56c741db9858
fanrui <fanr_cc@163.com>
2018-04-18 13:40:36 +0800

    项目配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css','~/assets/css/animate.css','~/assets/css/font-awesome.css','~/assets/css/normalize.css',],
   /*
   ** Add axios globally
   */
